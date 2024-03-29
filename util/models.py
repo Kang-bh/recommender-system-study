c8d79704fcb9c4eb3115dfe4982a6a582b2b215d
Kang-bh <kabyho3513@gmail.com>
2024-03-29 20:44:55 +0900

    add recommend_result class

--- util/models.py
+++ util/models.py
@@ -8,3 +8,9 @@ class Dataset:
     test: pd.DataFrame
     test_user2items: Dict[int, List[int]]
     item_content: pd.DataFrame
+
+
+@dataclasses.dataclass(frozen=True)
+class RecommendResult:
+    rating: pd.DataFrame
+    user2items: Dict[int, List[int]]
